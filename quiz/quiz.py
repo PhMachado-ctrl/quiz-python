f1a1e50bb88b5d508bb5cb77a8b85f2ab7ca79e4
PhMachado-ctrl <phmachado555@gmail.com>
2023-04-29 19:08:13 -0300

    Opção incorreta

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,12 +1,14 @@
 print("Começar o Quiz ? (S|N) \n")
-choose = input("R: ") #choose recebe o qye escrever no input
+choose = input("R: ") #choose get the input value
 
 if choose != "S" or choose !="s":
-    quit() #Fechar a execução
+    quit() #Finish the execution
 
 print("iniciando...")
 print("Qual Empresa Criou Grand Theft Auto ? \n (A) Rockstar Games \n (B) Ubisoft \n (C) Activision \n (D) EA \n")
 answer_1 = input("R: ")
 
 if answer_1 == "A" or answer_1 == "a":
-    print("Correct!")
+    print("Correto!")
+else:
+    print("incorreto!")
