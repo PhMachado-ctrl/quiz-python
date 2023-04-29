b5e4515207ce8f877ab913fa81ae93c4082ae5d1
PhMachado-ctrl <phmachado555@gmail.com>
2023-04-29 19:40:56 -0300

    Second-Question

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -8,8 +8,18 @@ if choose != "S":
 print("iniciando...")
 print("Qual Empresa Criou Grand Theft Auto ? \n (A) Rockstar Games \n (B) Ubisoft \n (C) Activision \n (D) EA \n")
 answer_1 = input("R: ")
+answer_1 = answer_1.upper()
 
-if answer_1 == "A" or answer_1 == "a":
+if answer_1 == "A":
+    print("Correto!")
+else:
+    print("incorreto!")
+
+print("Qual o nome do protagonista do jogo GTA San Andreas? \n (A) Carlos John \n (B) Carl Jonhson \n (C) Carl Jaqueline \n (D) Carlo Jonhson \n")
+answer_2 = input("R: ")
+answer_2 = answer_2.upper()
+
+if answer_2 == "A" :
     print("Correto!")
 else:
     print("incorreto!")
